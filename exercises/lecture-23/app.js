d0dd8de677f4c18392d0b30e289203e60baed4ac
reznichenkoaleksandra <reznichenkoaleksandra92@gmail.com>
2024-03-28 02:42:14 +0200

    lesson 23

---
+++ exercises/lecture-23/app.js
@@ -0,0 +1,145 @@
+const alert = document.querySelector('.alert');
+// task 1 Знайти на сторінці кнопку з класом btn-primary.
+// Призначте знайденому елементу подію onclick.Написати функцію обробки події onclick,
+// що додає CSS - клас alert - primary до елемента з id = alert та змінює значення властивості textContent
+// цього елемента на "A simple primary alert—check it out!".
+
+let btnPrimary = document.querySelector('.btn-primary');
+let alertContent = document.querySelector('.alert');
+btnPrimary.onclick = function () {
+    alertContent.className = 'alert-primary';
+    alertContent.textContent='A simple primary alert—check it out!'
+}
+
+// task 2 Знайти на сторінці кнопку з класом btn-secondary.
+// Додати до кнопки прослуховувач події "click".
+// Написати функцію обробки події click,
+// що додає CSS - клас alert - primary до елемента з id = alert  (secondary class)
+// та змінює значення властивості textContent цього елемента на
+// "A simple secondary alert—check it out!"
+
+let btnSecondary = document.querySelector('.btn-secondary');
+btnSecondary.addEventListener('click', () => {
+    alertContent.className = 'alert-secondary';
+    alertContent.textContent='A simple secondary alert—check it out!'
+} ) 
+
+// task 3 Знайти на сторінці кнопку з класом btn-success.
+// Додати до кнопки прослуховувач події "mouseover".
+// Написати функцію обробки події mouseover, що додає CSS - клас alert - success
+// до елемента з id = alert та змінює значення властивості textContent цього
+// елемента на "A simple success alert—check it out!"
+
+// Додати до кнопки прослуховувач події "mouseout". 
+// Написати функцію обробки події mouseout, що видаляє CSS - клас alert - success
+//  з елемента alert та змінює значення властивості 
+//  textContent цього елемента на пустий рядок.
+
+let btnSuccess = document.querySelector('.btn-success');
+btnSuccess.addEventListener('mouseover', () => {
+    alertContent.className = 'alert-succes';
+    alertContent.textContent='A simple success alert—check it out!'
+})
+btnSuccess.addEventListener('mouseout', () => {
+    alertContent.classList.remove('alert-succes');
+    alertContent.textContent = '';
+})
+
+// task 4 Знайти на сторінці кнопку з класом btn-danger.
+// Додати до кнопки прослуховувач події "focus".
+// Написати функцію обробки події focus, що додає CSS - клас alert - danger
+// до елемента з id = alert та змінює значення властивості
+// textContent цього елемента на "A simple danger alert—check it out!"
+// Додати до кнопки прослуховувач події "focusout".
+// Написати функцію обробки події focusout, що видаляє CSS - клас alert - danger
+// з елемента alert та змінює значення властивості textContent цього
+// елемента на пустий рядок.
+
+let btnDanger = document.querySelector('.btn-danger');
+
+btnDanger.addEventListener('focusin', () => {
+    alertContent.className = 'alert-danger';
+    alertContent.textContent='A simple danger alert—check it out!'
+})
+btnDanger.addEventListener('focusout', () => {
+    alertContent.classList.remove('alert-danger');
+    alertContent.textContent = '';
+})
+
+// task 5 Знайти на сторінці кнопки з класом btn-dark та btn-light.
+// Написати функцію toggleMode, що перемикає клас «dark - mode» елемента document.body
+// за допомогою методу classList.toggle().Одночасно при перемиканні класу dark - mode
+// потрібно приховувати або показувати відповідну кнопку.Якщо ввімкнено режим
+// dark - mode, показати кнопку btn - light та сховати кнопку dark - mode, і навпаки,
+// якщо вимкнено режим dark - mode, показати кнопку btn - dark та сховати кнопку btn - light
+
+let btnDark = document.querySelector('.btn-dark');
+let btnLight = document.querySelector('.btn-light');
+
+btnDark.onclick  = function toggleMode() {
+    document.body.classList.toggle('dark-mode');
+    if (document.body.classList.contains('dark-mode')) {
+        btnDark.classList.add('hide');
+        btnLight.classList.remove('hide');
+    }
+    else {
+        btnLight.classList.add('hide');
+        btnDark.classList.remove('hide');
+    }
+    return btnLight.onclick = function () {    
+        document.body.classList.remove('dark-mode');
+        if (document.body.classList.contains('dark-mode')) {
+            btnDark.classList.add('hide');
+            btnDark.classList.remove('hide');
+        } 
+        else {
+            btnLight.classList.add('hide');
+            btnDark.classList.remove('hide');
+    }
+    }
+}
+// чи можна написати це без другої частини? пробувала для обох кнопок налаштувати
+// подію , але тоді , якщо спочатку натиснути лайт - буде налаштована темна тема.
+// я б зробила це за допомогою однієї кнопки, але при натисканні, вона змінювала б свій контент
+// та її особистий стиль.
+
+// task 6 Знайти на сторінці кнопку з класом btn-info. Додати до кнопки прослуховувач події "keypress".
+// Написати функцію обробки події keypress, що перевіряє, чи є натиснута клавіша,
+// клавішею "Enter".Якщо це так, типову дію події потрібно скасовувати за допомогою методу event.preventDefault().
+// Після скасування дії за замовчуванням, додати CSS - клас alert - info до елемента з id = alert
+// та змінити значення властивості textContent
+// цього елемента на "A simple info alert—check it out!";
+
+let btnInfo = document.querySelector('.btn-info');
+btnInfo.addEventListener('keypress', (event) => {
+    if (event.key === 'Enter') {
+        event.preventDefault();
+        alert.classList.add('alert-info');
+        alert.textContent = 'A simple info alert—check it out!';
+    }
+});
+
+// task 7 Знайти на сторінці всі селектори .card.
+// Використовуючи цмкл for, вивести на консоль вміст кожного елемента з класом.card - title
+
+let cards = document.querySelectorAll('.card');
+
+for (let i = 0; i < cards.length; i++){
+   let title = document.querySelectorAll('.card-title');
+    console.log(title[i].textContent)
+}
+
+// task 8 Знайти на сторінці всі селектори .card. Використовуючи цикл for, знайти для
+// кожного елемента.card кнопку з класом.add - to - cart, додати для кожної кнопки
+// обробник події click, що викликає функцію, яка виводить на консоль
+// вміст кожного елемента з класом.card - title
+
+
+for (let n = 0; n < cards.length; n++){
+    let btnAddToCart = cards[n].querySelector('.add-to-cart');
+    // console.log(btnAddToCart.textContent);
+    btnAddToCart.addEventListener('click', () => {
+        let cardTitle = cards[n].querySelector('.card-title');
+        console.log(cardTitle.textContent)
+    })
+}
--- js/app.js
+++ js/app.js
@@ -12,10 +12,10 @@
 // }
 
 // section;
-console.dir(document.getElementById('hero'));
-let hero = document.getElementById('hero');
-let h1 = hero.querySelector('h1');
-console.dir(h1);
+// console.dir(document.getElementById('hero'));
+// let hero = document.getElementById('hero');
+// let h1 = hero.querySelector('h1');
+// console.dir(h1);
 
 // h1 = hero.getElementsByTagName('h1');
 // console.log(h1[0].innerText);
@@ -41,4 +41,148 @@ console.dir(h1);
 // console.log(imgs[0]);
 
 // imgs[0].setAttribute('src', '/img/images/product-7.jpg')
-// imgs[1].setAttribute('src', '/img/images/product-5-200px.jpg') 
+// imgs[1].setAttribute('src', '/img/images/product-5-200px.jpg')
+function Product(id, name, price) {
+  this.id = id;
+  this.name = name;
+  this.price = price;
+}
+
+function Cart(tax = 0.07, shipping = 0) {
+    this.tax = tax;
+    this.shipping = shipping;
+    let cart = [];
+    this.saveCart = function () {
+        console.log(cart);
+        function Item(id, price, amount) {
+            this.id = id;
+            this.price = price;
+            this.amount = amount;
+        }
+        this.addItemToCart = function () {
+
+            for (let item in cart) {
+                if (cart[item].id === product.id) {
+                    cart[item].amount += product.amount;
+                    this.saveCart();
+                    return;
+                }
+            }
+
+            let item = new Item(product.id, product.price, product.amount);
+            cart.push(item);
+            this.saveCart();
+        }
+        this.setCountForItem = function (id, amount) {
+            for (let i in cart) {
+                if (cart[i].id === id) {
+                    cart[i].amount = amount;
+                }
+            }
+        }
+        this.totalAmount = function () {
+            let total = 0;
+            for (let item in cart) {
+                total += cart[item].amount;
+            }
+            return total;
+        }
+        this.totalInCart = function () {
+            let total = 0;
+            for (let item in cart) {
+                total += cart[item].price * cart[item].amount
+            }
+            return (total * (1 + this.tax) + this.shipping);
+        }
+        this.remuveItemFromCart = function (id) {
+            for (let item in cart) {
+                if (cart[item].id === id) {
+                    cart[item].amount--;
+                    if (cart[item].amount === 0) {
+                        cart.splice(item, 1);
+                    }
+                    break;
+                }
+            }
+            this.saveCart();
+        }
+        this.remuveAllItemFromCart = function (id) {
+            for (let item in cart) {
+                if (cart[item].id === id) {
+                    cart.splice(item, 1);
+                    break;
+                }
+            }
+            this.saveCart();
+        }
+        this.clearCart = function () {
+            cart = [];
+            this.saveCart();
+        }
+    }
+}
+
+let shoppingCart = new Cart(tax = 0.07, shipping = 0);
+
+
+function main() {
+    
+}
+
+if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', () => {
+        main();
+    })
+} else {
+    main();
+}
+
+const productContainer = document.querySelector('.product-container');
+
+let addToCart = productContainer.querySelector('.add-to-cart');
+let showDetail = productContainer.querySelector('.show-details');
+// console.dir(addToCart);
+addToCart.addEventListener('click', () => {
+    // console.dir(addToCart);
+})
+const dialog = document.getElementById('detail');
+showDetail.addEventListener('click', () => {
+   
+    dialog.showModal();
+})
+dialog.querySelector('.close').addEventListener('click', () => {
+    dialog.close();
+})
+
+let addToCartButtons = productContainer.querySelectorAll('.add-to-cart');
+for (let item of addToCartButtons) {
+    item.addEventListener('click', () => {
+        console.log(item);
+    })
+}
+
+function CardProduct(item) {
+    this.item = item;
+    let addToCart = this.item.querySelector('.add-to-cart');
+    addToCart.addEventListener('click', function (event) {
+        console.log(event.target)
+
+        let parent = event.target.closest('.product');
+        let price = parent.querySelector('.product-price').innerText;
+        let name = parent.querySelector('.product-name').innerText;
+        let id = parent.querySelector('.product-name').getAttribute('id');
+        console.log( id , name , price)
+
+        let product = new Product(id, name, price)
+        product = {...product, amount:1}
+        console.log(product);
+        shoppingCart.addItemToCart(product);
+        console.log(shoppingCart.totalAmount())
+        console.log(shoppingCart.totalInCart())
+    });
+
+}
+    let productCards = productContainer.querySelectorAll('.product');
+for (let item of productCards) {
+    new CardProduct(item);
+}
